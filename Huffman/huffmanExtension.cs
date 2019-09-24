6723bfb3c649f7ddf1842a9d2f62c5d7f3e628c6
Oskar <oskarcali18@gmail.com>
2019-09-24 13:12:18 -0500

    Extension de Huffman
    Nodos que no tienen padre

---
+++ Huffman/huffmanExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Huffman
+{
+    static class huffmanExtension
+    {
+        static List<Nodo> sinPadres(this List<Nodo> arbol)
+        {
+            List<Nodo> sinPadres = new List<Nodo>();
+            foreach (var nodo in arbol)
+            {
+                if (nodo.Padre == null)
+                {
+                    sinPadres.Add(nodo);
+                }
+            }
+
+            return sinPadres;
+        }
+    }
+}
