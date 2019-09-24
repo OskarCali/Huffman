6762c6dde7d433e5e990aeb93d83101ce2a75593
Oskar <oskarcali18@gmail.com>
2019-09-24 12:06:43 -0500

    Clase Huffman
    Metodo para crear el arbol inicial

---
+++ Huffman/Huffman.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Huffman
+{
+    class Huffman
+    {
+        List<Nodo> crearNodos(Hashtable caracteres)
+        {
+            List<Nodo> arbol = new List<Nodo>();
+
+            foreach (DictionaryEntry entry in caracteres)
+            {
+                Element element = entry.Value as Element;
+                arbol.Add(new Nodo((int)entry.Key, element.Caracter.ToString(), element.Ocurrencias));
+            }
+
+            return arbol.OrderBy(x => x.NodoId).OrderByDescending(x => x.Valor).ToList();
+        }
+    }
+}
