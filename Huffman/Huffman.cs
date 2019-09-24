b1f2c2b67467fa805f588112ac064b55485ef521
Oskar <oskarcali18@gmail.com>
2019-09-24 13:11:49 -0500

    Valor compuesto
    Creacion de los padres (arbol)

--- Huffman/Huffman.cs
+++ Huffman/Huffman.cs
@@ -21,5 +21,60 @@ namespace Huffman
 
             return arbol.OrderBy(x => x.NodoId).OrderByDescending(x => x.Valor).ToList();
         }
+
+        void valorCompuesto(List<Nodo> arbol)
+        {
+            while (arbol.sinPadres().Count > 1)
+            {
+                List<Nodo> sinPadres = arbol.sinPadres();
+                Nodo der = null;
+                Nodo izq = null;
+
+                if (sinPadres.Count != 2)
+                {
+                    der = sinPadres.FindLast(x => true);
+
+                    for (int i = (sinPadres.Count - 1); i >= 0; i--)
+                    {
+                        if (sinPadres[i].Valor < der.Valor)
+                        {
+                            der = sinPadres[i];
+                        }
+                    }
+
+                    List<Nodo> sinSeleccionar = arbol.sinPadres().quitarNodo(der);
+                    izq = sinSeleccionar.FindLast(x => true);
+
+                    for (int i = (sinSeleccionar.Count - 1); i >= 0; i--)
+                    {
+                        if (sinSeleccionar[i].Valor < izq.Valor)
+                        {
+                            izq = sinSeleccionar[i];
+                        }
+                    }
+                }
+                else
+                {
+                    if (sinPadres[0].Valor > sinPadres[1].Valor)
+                    {
+                        izq = sinPadres[0];
+                        der = sinPadres[1];
+                    }
+                    else
+                    {
+                        izq = sinPadres[1];
+                        der = sinPadres[0];
+                    }
+                }
+
+                der.Binario = 0;
+                izq.Binario = 1;
+
+                Nodo padre = new Nodo((arbol.Count + 1), der.Valor.ToString() + izq.Valor.ToString(), (der.Valor + izq.Valor), der, izq);
+                arbol.FirstOrDefault(x => x == der).Padre = padre;
+                arbol.FirstOrDefault(x => x == izq).Padre = padre;
+                arbol.Add(padre);
+            }
+        }
     }
 }
