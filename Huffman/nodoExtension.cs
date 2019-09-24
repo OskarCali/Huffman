fb84b835a53c380426c94ce8b1b85b37909d97de
Oskar <oskarcali18@gmail.com>
2019-09-24 13:28:09 -0500

    Extension Nodo
    Metodo para generar la palabra codigo de un nodo

---
+++ Huffman/nodoExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Huffman
+{
+    static class nodoExtension
+    {
+        public static string codigo(this Nodo nodo, Nodo root)
+        {
+            string code = "";
+
+            if (nodo.Padre != null)
+            {
+                code += nodo.Padre.codigo(root);
+            }
+
+            if (nodo != root)
+            {
+                code += nodo.Binario;
+            }
+
+            return code;
+        }
+    }
+}
