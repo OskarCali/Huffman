af502d7b084260c8780af21f68cf8848d762fcce
Oskar <oskarcali18@gmail.com>
2019-09-24 02:18:37 -0500

    Analisis de Cantidad de Informacion
    Clases que se usan para la cantidad de informacion de un texto

---
+++ Huffman/Element.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Huffman
+{
+    /// <summary>
+    /// Definicion de la clase Element para la Cantidad de Informacion
+    /// </summary>
+    class Element
+    {
+        // Representa el caracter del analisis de la Cantidad de Informacion
+        public char Caracter { get; set; }
+        // Representa el numero de ocurrencias que tiene el caracter.
+        public int Ocurrencias { get; set; }
+    }
+}
---
+++ Huffman/elementExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Huffman
+{
+    /// <summary>
+    /// Definicion de clase elementExtension para extensiones de la clase ELement
+    /// </summary>
+    static class elementExtension
+    {
+        /// <summary>
+        /// Metodo actualizar para aumentar el numero de ocurrencias de un elemento
+        /// </summary>
+        /// <param name="element">Elemento que se actualizara</param>
+        /// <returns>Elemento actualizado en concurrencias</returns>
+        public static Element actualizar(this Element element)
+        {
+            element.Ocurrencias += 1;
+            return element;
+        }
+    }
+}
