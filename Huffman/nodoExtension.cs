afbf44e902a6688550c66adcccb37a7936019a83
Oskar <oskarcali18@gmail.com>
2019-09-24 20:51:43 -0500

    Actualizar extension Nodo
    Metodo para crear el arbol de la UI

--- Huffman/nodoExtension.cs
+++ Huffman/nodoExtension.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Huffman
 {
@@ -33,5 +34,47 @@ namespace Huffman
 
             return code;
         }
+
+        /// <summary>
+        /// Metodo para crear arbol para la UI
+        /// </summary>
+        /// <param name="nodo">Nodo root o donde se inicia a aconstruir el arbol</param>
+        /// <param name="iniciales">Lista de nodos iniciales</param>
+        /// <returns>Lista de nodos para el arbol visual de la UI</returns>
+        public static List<TreeNode> arbolUI(this Nodo nodo, List<Nodo> iniciales)
+        {
+            List<TreeNode> treeNodes = new List<TreeNode>();
+            List<TreeNode> childs = new List<TreeNode>();
+            TreeNode treeNode;
+
+            // Creacion del nodo segun su tipo
+            if (iniciales.Contains(nodo))
+            {
+                treeNode = new TreeNode(nodo.Nombre + " - " + nodo.Codigo);
+            }
+            else
+            {
+                treeNode = new TreeNode(nodo.Nombre);
+            }
+
+            // Verificar si tiene camino a la izquierda
+            if (nodo.Izquierdo != null)
+            {
+                childs = nodo.Izquierdo.arbolUI(iniciales);
+                treeNode.Nodes.AddRange(childs.ToArray());
+            }
+
+            // Verificar si tiene camino a la derecha
+            if (nodo.Derecho != null)
+            {
+                childs = nodo.Derecho.arbolUI(iniciales);
+                treeNode.Nodes.AddRange(childs.ToArray());
+            }
+
+            treeNodes.Add(treeNode);
+
+            return treeNodes;
+        }
+
     }
 }
